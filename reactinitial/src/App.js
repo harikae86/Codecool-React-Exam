dda2ca7da551f92b499fa184860f7d0c58b40f8d
harikae86 <harmath.esztike@gmail.com>
2022-03-17 09:48:46 +0100

    fetch from api

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -1,12 +1,31 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import LoadingMask from "./components/LoadingMask";
+
+const fetchFromApi = () => {
+  const response = fetch("https://demoapi.com/api/laptop").then((res) =>
+    res.json()
+  );
+  return response;
+};
 
 const App = () => {
+  const [laptops, setLaptops] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchFromApi().then((data) => {
+      setLaptops(data);
+      setLoading(false);
+    });
+  }, []);
+  console.log(laptops);
 
   return (
     <div>
-      Todo...
+      <h1>Laptops</h1>
+      {loading && <LoadingMask />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
